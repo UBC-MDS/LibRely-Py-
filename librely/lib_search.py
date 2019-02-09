edf69a8e5f8022b09e0e1f898a166e844cb1cbcd
Zixin Zhang <zxzzhangg@gmail.com>
2019-02-08 22:38:51 -0800

    updated the first function and test

---
+++ librely/lib_search.py
@@ -0,0 +1,24 @@
+# Feb 8th, 2019
+# Zixin Zhang
+# This script is the function in Python to find the dependencies used in a .py script
+
+
+import pandas as pd
+import numpy as np
+
+
+def lib_search(input_path):
+
+    """
+    This function takes an input Python script and return the libraries that used in this script
+
+    Parameters
+    ---------
+    input_path(string): the path to the input .py script
+
+    Return
+    --------
+    depen(list): a list that stores all the dependencies
+    """
+
+    return depen
