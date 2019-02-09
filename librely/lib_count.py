9251467eb64ed6e65713bb6cef8238c08c44e3ba
Alex Hope <ahope@ualberta.ca>
2019-02-09 00:18:36 -0800

    edits to lib_count

--- librely/lib_count.py
+++ librely/lib_count.py
@@ -1,12 +1,7 @@
 import pandas as pd
 import numpy as np
 
-# Feb 9th, 2019
-# Zixin Zhang, Alex Hope, Aaron Quinton
-# This script tests the function for lib_count.py
-#
-# input: a path leads to the .py script
-# output: a list that contains all the dependencies
+
 
 library(pandas)
 library(numpy)
@@ -24,7 +19,7 @@ def lib_count(input_path):
 
     Return
     ----------
-    depen(DataFrame): A DataFrame that stores all the dependencies, functions
+    depen_func_dataframe(DataFrame): A DataFrame that stores all the dependencies, functions
     and function counts
     """
 
