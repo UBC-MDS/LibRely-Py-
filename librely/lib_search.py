126b30830dfaaa6f43281c7bf8600f97d999853c
Alex Hope <ahope@ualberta.ca>
2019-02-10 20:59:35 -0800

    code for first function

--- librely/lib_search.py
+++ librely/lib_search.py
@@ -10,15 +10,30 @@ import numpy as np
 def lib_search(input_path):
 
     """
-    This function takes an input Python script and return the libraries that used in this script
+    This function takes in a Python script (.py) and returns a dataframe
+    containing the libraries and functions within the script as well as a
+    use-count of the functions
 
     Parameters
-    ---------
-    input_path(string): the path to the input .py script
+    ----------
+    input_path(string): The path to the input .py script
 
     Return
-    --------
-    depen(list): a list that stores all the dependencies
+    ----------
+    depen_func_dataframe(DataFrame): A DataFrame that stores all the dependencies, functions
+    and function counts
     """
 
-    return depen
+    dependency=[] #### Set an empty list containing dependencies
+    in_file=open(filepath,"r") ### Open File
+
+    lines = in_file.readlines() ### Read Lines of File
+
+    for line in lines: ### For Loop for Each Line
+        thisline=line.split(" ") ### Split Every Word in the Line
+
+        if thisline[0] == 'import': ### If the line starts with 'import'
+            edits = thisline[1].replace('\n', '') #### Grab the word after it
+            dependency.append(edits) #### Append the word into dependency list
+
+    return dependency
